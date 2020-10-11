b0474a6b864a68052d80a884b8517961a77c0195
Umang Kharecha <UKharecha>
2020-10-11 12:24:48 +0200

    update game.py

--- game.py
+++ game.py
@@ -1,30 +1,30 @@
 import random
+
 user = {
-    "Archer": 
- {
-        "Name": "",
-        "Age": 0,
-        "Sex": "",
-        "Health": 100,
-        "KD_Ratio": 0,
-        "Weapon": "arrows",
-        "Ammunition": 100
+    "Archer":
+        {
+            "Name": "",
+            "Age": 0,
+            "Sex": "",
+            "Health": 100,
+            "KD_Ratio": 0,
+            "Weapon": "arrows",
+            "Ammunition": 100
 
- },
+        },
 
-    "Shooter": 
-{
-        "Name": "",
-        "Age" :0,
-        "Sex": "",
-        "Player_Type": "",
-        "Health": 100,
-        "KD_Ratio": 0,
-        "Weapon": "bullets",
-        "Ammunition": 50
- } 
+    "Shooter":
+        {
+            "Name": "",
+            "Age": 0,
+            "Sex": "",
+            "Player_Type": "",
+            "Health": 100,
+            "KD_Ratio": 0,
+            "Weapon": "bullets",
+            "Ammunition": 50
+        }
 }
-
 
 game_img = """
         ------------------------------------------------------
@@ -40,6 +40,7 @@ game_img = """
         ------------------------------------------------------
       """
 
+
 class Player:
 
     def __init__(self, archer=user["Archer"], shooter=user["Shooter"]):
@@ -54,22 +55,20 @@ class Player:
         player2_name = input(f"Who is Player 2?\n")
         player2_age = int(input(f"Hi {player2_name}. How old are you?\n"))
         player2_sex = input("Enter your gender in M/F\n")
-        
+
         randomize = random.choice(player_type)
 
         if randomize == archer:
-            archer["Name"].update(player1_name)
-            archer["Age"].update(player1_age)
-            archer["Sex"].update(player1_sex)
+            a = {archer["Name"]: player1_name, archer["Age"]: player1_age, archer["Sex"]: player1_sex}
+            user.update(a)
             print(
                 f"""
                 Hi {archer["Name"]} you are the Archer.
                 You will start with {archer['Ammunition']} {archer['Weapon']}.
                 You have {archer['Health']}% health. Play carefully!
                 """)
-            shooter["Name"].update(player2_name)
-            shooter["Age"].update(player2_age)
-            shooter["Sex"].update(player2_sex)
+            s = {shooter["Name"]: player2_name, shooter["Age"]: player2_age, shooter["Sex"]: player2_sex}
+            user.update(s)
             print(
                 f"""
                 Hi {shooter["Name"]} you are the Shooter.
@@ -78,33 +77,29 @@ class Player:
                 """)
 
         if randomize == shooter:
-            shooter["Name"].update(player1_name)
-            shooter["Age"].update(player1_age)
-            shooter["Sex"].update(player1_sex)
+            s = {shooter["Name"]: player1_name, shooter["Age"]: player1_age, shooter["Sex"]: player1_sex}
+            user.update(s)
             print(
                 f"""
-                Hi {shooter["Name"]} you are the Shooter.
-                You will start with {shooter['Ammunition']} {shooter['Weapon']}.
-                You have {shooter['Health']}% health. Play carefully!
-                """)
-            archer["Name"].update(player2_name)
-            archer["Age"].update(player2_age)
-            archer["Sex"].update(player2_sex)
+                            Hi {shooter["Name"]} you are the Shooter.
+                            You will start with {shooter['Ammunition']} {shooter['Weapon']}.
+                            You have {shooter['Health']}% health. Play carefully!
+                            """)
+
+            a = {archer["Name"]: player2_name, archer["Age"]: player2_age, archer["Sex"]: player2_sex}
+            user.update(a)
             print(
                 f"""
-                Hi {archer["Name"]} you are the Archer.
-                You will start with {archer['Ammunition']} {archer['Weapon']}.
-                You have {archer['Health']}% health. Play carefully!
-                """)
+                            Hi {archer["Name"]} you are the Archer.
+                            You will start with {archer['Ammunition']} {archer['Weapon']}.
+                            You have {archer['Health']}% health. Play carefully!
+                            """)
 
 
 class Fight(Player):
 
     def __init__(self):
         pass
-            
-
-
 
 # print(team1.health())
 
