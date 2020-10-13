356886ed2193a79878aa12fd6114625c1e874e2b
schikani <shivangchikani1@gmail.com>
2020-10-13 23:36:24 +0200

    Update game.py and add sound files

--- game.py
+++ game.py
@@ -1,18 +1,14 @@
 import random
 
-user = {
-    "Archer":
-        {
+Archer = {
             "Name": "",
             "Health": 100,
             "KD_Ratio": 0,
             "Weapon": "arrows",
             "Ammunition": 100
+        }
 
-        },
-
-    "Shooter":
-        {
+Shooter = {
             "Name": "",
             "Player_Type": "",
             "Health": 100,
@@ -20,7 +16,9 @@ user = {
             "Weapon": "bullets",
             "Ammunition": 50
         }
-}
+
+
+
 
 game_img = """
         ------------------------------------------------------
@@ -36,19 +34,17 @@ game_img = """
         ------------------------------------------------------
       """
 
-
 class Player:
 
-    def __init__(self):
+    def __init__(self, archer, shooter):
         print(game_img)
-        self.archer = user["Archer"]
-        self.shooter = user["Shooter"]
+        self.archer = archer
+        self.shooter = shooter
         player_type = ["archer", "shooter"]
 
         player1_name = input(f"Who is Player 1?\n")
 
         player2_name = input(f"Who is Player 2?\n")
-
 
         randomize = random.choice(player_type)
 
@@ -112,49 +108,75 @@ def fight(archer, shooter):
         else:
 
             num_arrows_fire = random.randint(1, 10)
-            archer_turn = str(input(
-                f"""
-                {archer['Name']}, The Computer chose to attack with {num_arrows_fire} {archer['Weapon']} at {shooter['Name']}.
-                Do you want to shoot? Type Y/N.\n
-
-                """))
-            if archer_turn in ("y", "Y"):
-                damage_by_archer = num_arrows_fire * 2
-                update_ammo_archer = {"Ammuntion":archer["Ammunition"] - num_arrows_fire}
-                update_health_shooter = {"Health":shooter["Health"] - damage_by_archer}
-                archer.update(update_ammo_archer)
-                shooter.update(update_health_shooter)
-                print(
+            if archer["Ammunition"] < num_arrows_fire:
+                num_arrows_fire = archer["Ammunition"]
+                last_arms_archer = input(str(
                     f"""
-                    Shot {num_arrows_fire} {archer['Weapon']} at {shooter['Name']}
-                    You have {archer["Ammuntion"]} arrows left with {archer["Health"]}% health.
-                    {shooter['Name']} has {shooter['Health']}% health left."
+                    You have {num_arrows_fire} arrows left. Do want to shoot at {shooter['Name']}
+                    \n"""))
+                if last_arms_archer in ("y", "Y"):
+                    damage_by_archer = num_arrows_fire * 2
+                    update_ammo_archer = {"Ammunition": archer["Ammunition"] - num_arrows_fire}
+                    update_health_shooter = {"Health": shooter["Health"] - damage_by_archer}
+                    archer.update(update_ammo_archer)
+                    shooter.update(update_health_shooter)
 
-                    """)
+            if archer["Ammunition"] > num_arrows_fire and archer["Health"] > 0:
+                archer_turn = str(input(
+                    f"""
+                    {archer['Name']}, The Computer chose to attack with {num_arrows_fire} {archer['Weapon']} at {shooter['Name']}. 
+                    Do you want to shoot? Type Y/N.\n
+    
+                    """))
+                if archer_turn in ("y", "Y"):
+                    damage_by_archer = num_arrows_fire * 2
+                    update_ammo_archer = {"Ammunition": archer["Ammunition"] - num_arrows_fire}
+                    update_health_shooter = {"Health": shooter["Health"] - damage_by_archer}
+                    archer.update(update_ammo_archer)
+                    shooter.update(update_health_shooter)
+                    print(
+                        f"""
+                        Shot {num_arrows_fire} {archer['Weapon']} at {shooter['Name']}
+                        You have {archer["Ammunition"]} arrows left with {archer["Health"]}% health.
+                        {shooter['Name']} has {shooter['Health']}% health left.
+    
+                        """)
 
             num_bullets_fire = random.randint(1, 10)
-            shooter_turn = str(input(
-                f"""
-                {shooter['Name']}, The Computer chose to attack with {num_bullets_fire} {shooter['Weapon']} at {archer['Name']}.
-                Do you want to shoot? Type Y/N.\n 
-
-                 """))
-            if shooter_turn in ("y", "Y"):
-                damage_by_shooter = num_bullets_fire 
-                update_ammo_shooter = {"Ammunition":shooter["Ammunition"] - num_bullets_fire}
-                update_health_archer = {"Health":archer["Health"] - damage_by_shooter}
-                shooter.update(update_ammo_shooter)
-                archer.update(update_health_archer)
-                print(
+            if shooter["Ammunition"] < num_bullets_fire:
+                num_bullets_fire = shooter["Ammunition"]
+                last_arms_shooter = input(str(
                     f"""
-                    Shot {num_bullets_fire} {shooter['Weapon']} at {archer['Name']}
-                    You have {shooter["Ammunition"]} bullets left with {shooter["Health"]}% health.
-                    {archer['Name']} has {archer['Health']}% health left."
+                    You have {num_bullets_fire} bullets left. Do want to shoot at {archer['Name']}
+                    \n"""))
+                if last_arms_shooter in ("y", "Y"):
+                    damage_by_shooter = num_bullets_fire
+                    update_ammo_shooter = {"Ammunition": shooter["Ammunition"] - num_bullets_fire}
+                    update_health_archer = {"Health": archer["Health"] - damage_by_shooter}
+                    shooter.update(update_ammo_shooter)
+                    archer.update(update_health_archer)
 
-                    """)
+            if shooter["Ammunition"] > num_bullets_fire and shooter["Health"] > 0:
+                shooter_turn = str(input(
+                    f"""
+                    {archer['Name']}, The Computer chose to attack with {num_bullets_fire} {shooter['Weapon']} at {archer['Name']}. 
+                    Do you want to shoot? Type Y/N.\n
+    
+                    """))
+                if shooter_turn in ("y", "Y"):
+                    damage_by_shooter = num_bullets_fire
+                    update_ammo_shooter = {"Ammunition": shooter["Ammunition"] - num_bullets_fire}
+                    update_health_archer = {"Health": archer["Health"] - damage_by_shooter}
+                    shooter.update(update_ammo_shooter)
+                    archer.update(update_health_archer)
+                    print(
+                        f"""
+                        Shot {num_bullets_fire} {shooter['Weapon']} at {archer['Name']}
+                        You have {shooter["Ammunition"]} bullets left with {shooter["Health"]}% health.
+                        {archer['Name']} has {archer['Health']}% health left.
+    
+                        """)
 
-        
 
-
-player1 = Player()
-fight(archer=user["Archer"], shooter=user["Shooter"])
+play = Player(archer=Archer, shooter=Shooter)
+fight(archer=play.archer, shooter=play.shooter)
